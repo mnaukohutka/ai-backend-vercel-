11de25c9038dd2ece4f9208c2ff8996a8fc14961
mnaukohutka <zpravy.robotu@outlook.cz>
2025-10-08 09:01:39 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+export default {
+  reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+        ],
+      },
+    ]
+  },
+}
